acc0d0c5a9f8b2e726f4bb8f171125e1255df014
Shivam Singh <shivamsingh262@gmail.com>
2015-07-28 20:51:11 +0530

    Updates

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,12 @@
 mainApp.controller("myController", function($scope,instagram,tag,$routeParams) {
       $scope.pics = [];
-      //$scope.have = [];
 	  $scope.tags = [];
 	  $scope.look = $routeParams.param;
       $scope.orderBy = "-likes.count";
       $scope.getMore = function() {
         instagram.fetchPopular($scope.look,function(data) {
             for(var i=0; i<data.length; i++) {
-              //if (typeof $scope.have[data[i].id]==="undefined") {
                 $scope.pics.push(data[i]) ;
-                //$scope.have[data[i].id] = "1";
-              //}
             }
         });
       };
@@ -23,5 +19,4 @@ mainApp.controller("myController", function($scope,instagram,tag,$routeParams) {
 		});
 	  };
 	  $scope.getMore();
-	  //$scope.search();
     });
